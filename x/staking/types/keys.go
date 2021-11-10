dd9108c2aac59f7e710b12e52d84d0ad1fc0536c
Aditya Sripal <adityasripal@gmail.com>
2021-11-10 12:17:21 +0100

    change transient key name

--- x/staking/types/keys.go
+++ x/staking/types/keys.go
@@ -21,7 +21,7 @@ const (
 	StoreKey = ModuleName
 
 	// TStoreKey is the string transient store representation
-	TStoreKey = ModuleName
+	TStoreKey = "transient_staking"
 
 	// QuerierRoute is the querier route for the staking module
 	QuerierRoute = ModuleName
