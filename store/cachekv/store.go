15558a8a60b5051c119c3203c4bfcb0a15d55f63
ValarDragon <dojha12@gmail.com>
2021-12-24 19:38:37 -0600

    Fix delete datarace

--- store/cachekv/store.go
+++ store/cachekv/store.go
@@ -265,7 +265,7 @@ func (store *Store) setCacheValue(key, value []byte, deleted bool, dirty bool) {
 	}
 	if deleted {
 		store.deleted[keyStr] = struct{}{}
-	} else {
+	} else if dirty {
 		delete(store.deleted, keyStr)
 	}
 	if dirty {
