076cd581e529c73335f63004d1643a8d496c30c7
Aaron Craelius <aaronc@users.noreply.github.com>
2022-02-25 16:54:14 -0500

    WIP

--- app/event/service.go
+++ app/event/service.go
@@ -6,24 +6,30 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-type Service interface {
-}
-
-func Emit(context.Context, proto.Message) error {
-	return nil
-}
-
-func EmitSilently(context.Context, proto.Message) error {
-	return nil
-}
-
-func EmitLegacy(ctx context.Context, eventType string, attrs ...LegacyEventAttribute) error {
-	return nil
+type Manager interface {
+	Emit(proto.Message) error
+	EmitSilently(proto.Message)
+	EmitLegacy(ctx context.Context, eventType string, attrs ...LegacyEventAttribute) error
 }
 
 type LegacyEventAttribute struct {
 	Key, Value string
 }
+
+func GetManager(ctx context.Context) Manager {
+	mgr, ok := ctx.Value(managerContextKey{}).(Manager)
+	if !ok {
+		return noopEventMgr{}
+	}
+
+	return mgr
+}
+
+func WithManager(ctx context.Context, manager Manager) context.Context {
+	return context.WithValue(ctx, managerContextKey{}, manager)
+}
+
+type managerContextKey struct{}
 
 type Hook struct {
 	EventType proto.Message
@@ -33,3 +39,15 @@ type Hook struct {
 type Hooks []Hook
 
 func (Hooks) IsOnePerModuleType() {}
+
+type noopEventMgr struct{}
+
+func (n noopEventMgr) Emit(message proto.Message) error { return nil }
+
+func (n noopEventMgr) EmitSilently(message proto.Message) {}
+
+func (n noopEventMgr) EmitLegacy(ctx context.Context, eventType string, attrs ...LegacyEventAttribute) error {
+	return nil
+}
+
+var _ Manager = noopEventMgr{}
