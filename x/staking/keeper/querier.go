10616879a2091fa03fc1620c005cebb90c9c334c
Jehan <hi@jehan.email>
2022-05-10 01:56:36 -0700

    Finish staking hooks merge (#11677)
    * allow stopping and completing of redelegations
    
    * refactor to remove BeforeUnbondingDelegationEntryComplete hook and notes for validator unbonding
    
    * WIP rough draft of validator unbonding hooks
    
    * add many of marius's suggested changes
    
    * More review changes
    
    * unbonding delegation tests pass
    
    * WIP adding redelegation tests
    
    * WIP redelegation tests work
    
    * unbondingDelegation and redelegation tests pass and cleanup
    
    * WIP validator unbonding tests almost pass
    
    * tests for all new functionality pass
    
    * fix index deleting logic
    
    * clean up TODOs
    
    * fix small logic bug
    
    * fix slashing tests
    
    * Rename statements containing 'UnbondingOp' to 'Unbond' in code, docs and proto files
    
    Co-authored-by: Simon <simon.ntz@gmail.com>

--- x/staking/keeper/querier.go
+++ x/staking/keeper/querier.go
@@ -512,6 +512,8 @@ func RedelegationsToRedelegationResponses(
 				entry.SharesDst,
 				entry.InitialBalance,
 				val.TokensFromShares(entry.SharesDst).TruncateInt(),
+				entry.UnbondingOnHold,
+				entry.UnbondingId,
 			)
 		}
 
