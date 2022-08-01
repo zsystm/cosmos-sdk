8697bbc2808e272c4b9eadb83ddd9d419712f8cd
Marius Poke <marius.poke@posteo.de>
2022-08-01 13:04:51 +0200

    Revert "fix: make ModuleAccountInvariants pass for IS SDK changes (#12554)" (#12782)
    This reverts commit 67c81635b48358eea240a881c7f5d88e4f06616e.

--- x/staking/keeper/slash_test.go
+++ x/staking/keeper/slash_test.go
@@ -132,7 +132,7 @@ func TestSlashRedelegation(t *testing.T) {
 	// set a redelegation with an expiration timestamp beyond which the
 	// redelegation shouldn't be slashed
 	rd := types.NewRedelegation(addrDels[0], addrVals[0], addrVals[1], 0,
-		time.Unix(5, 0), sdk.NewInt(10), sdk.NewDec(10), 1)
+		time.Unix(5, 0), sdk.NewInt(10), sdk.NewDec(10), false, 1)
 
 	app.StakingKeeper.SetRedelegation(ctx, rd)
 
@@ -391,7 +391,7 @@ func TestSlashWithRedelegation(t *testing.T) {
 	// set a redelegation
 	rdTokens := app.StakingKeeper.TokensFromConsensusPower(ctx, 6)
 	rd := types.NewRedelegation(addrDels[0], addrVals[0], addrVals[1], 11,
-		time.Unix(0, 0), rdTokens, rdTokens.ToDec(), 1)
+		time.Unix(0, 0), rdTokens, rdTokens.ToDec(), false, 1)
 	app.StakingKeeper.SetRedelegation(ctx, rd)
 
 	// set the associated delegation
@@ -545,7 +545,7 @@ func TestSlashBoth(t *testing.T) {
 	rdATokens := app.StakingKeeper.TokensFromConsensusPower(ctx, 6)
 	rdA := types.NewRedelegation(addrDels[0], addrVals[0], addrVals[1], 11,
 		time.Unix(0, 0), rdATokens,
-		rdATokens.ToDec(), 1)
+		rdATokens.ToDec(), false, 1)
 	app.StakingKeeper.SetRedelegation(ctx, rdA)
 
 	// set the associated delegation
