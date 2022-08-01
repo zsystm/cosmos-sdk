cf208ae6471f268af3b9de509c6fb6512d206e21
Marius Poke <marius.poke@posteo.de>
2022-08-01 15:49:09 +0200

    fix: make ModuleAccountInvariants pass for IS SDK changes (#12783)
    * fix bug breaking ModuleAccountInvariants
    
    * set UnbondingOnHold to false explicitly

--- x/staking/keeper/querier.go
+++ x/staking/keeper/querier.go
@@ -512,7 +512,6 @@ func RedelegationsToRedelegationResponses(
 				entry.SharesDst,
 				entry.InitialBalance,
 				val.TokensFromShares(entry.SharesDst).TruncateInt(),
-				entry.UnbondingOnHold,
 				entry.UnbondingId,
 			)
 		}
